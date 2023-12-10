0f9f0f67b217b0d67b1f8436fbb21049453ffdaf
hypnos <hypnos@debian-BULLSEYE-live-builder-AMD64>
2023-12-11 03:00:14 +0400

    Refactor structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"gitlab.com/golanggin/initial/shadow/internal/routes"
+	"gitlab.com/golanggin/initial/shadow/internal/http/routes"
 	"gitlab.com/golanggin/initial/shadow/pkg/utils"
 )
 
